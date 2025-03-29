9509205ae39b5a23f87e2f5a29695e2fea7fc7ec
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-03-29 09:19:15 -0300

    feat: layout login google

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import GoogleLoginButton from '../components/GoogleLoginButton.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="layout">
+    <header class="header">
+      <h1>Welcome to the Home Page</h1>
+    </header>
+
+    <div class="main-content">
+      <GoogleLoginButton />
+    </div>
+
+    <footer class="footer">
+      <p>&copy; 2025 Your Company</p>
+    </footer>
+  </div>
 </template>
