f6ce92d0975bf65cd1152ab8a439db753036d222
Caue Assmann <caue@madesa.com>
2025-04-02 13:45:58 -0300

    feat: complete registration view

---
+++ src/views/CompleteRegistrationView.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import '@/styles/forms.scss';
+import { useToast } from '@/composables/useToast';
+import { useValidation } from '@/composables/useUserValidation';
+import { userService } from '@/services/user.service';
+import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
+const { errors, validateField, validateAll } = useValidation();
+
+const route = useRoute();
+
+const form = reactive({
+  cpf: '',
+  name: '',
+  birthday: ''
+});
+
+const submitForm = async () => {
+  if (!validateAll(form)) return
+
+  const response = await userService.completeRegistration({
+    user: String(route.params.id),
+    ...form
+  })
+  
+  if(!response.success) {
+    useToast().showToast("Erro ao completar o cadastro", "error");  
+    return;
+  }
+};
+</script>
+<template>
+  <div class="form-wrapper">
+    <div class="form-container">
+      <h2>Complete seu cadastro</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="cpf">CPF</label>
+          <input type="text" id="cpf" v-model="form.cpf" @input="validateField('cpf', form.cpf)" required v-maska data-maska="###.###.###-##" data-maska-tokens="X:\d"/>
+          <span class="error" v-if="errors.cpf">{{ errors.cpf }}</span>
+        </div>
+        
+        <div class="form-group">
+          <label for="name">Nome</label>
+          <input type="text" id="name" v-model="form.name" @input="validateField('name', form.name)" required />
+          <span class="error" v-if="errors.name">{{ errors.name }}</span>
+        </div>
+        
+        <div class="form-group">
+          <label for="birthday">Data de Nascimento</label>
+          <input type="date" id="birthday" v-model="form.birthday" @input="validateField('birthday', form.birthday)" required />
+          <span class="error" v-if="errors.birthday">{{ errors.birthday }}</span>
+        </div>
+        
+        <button type="submit">Registrar</button>
+      </form>
+    </div>
+  </div>
+</template>
