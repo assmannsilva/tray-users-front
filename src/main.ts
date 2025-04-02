11a6c1b4a3163e5b2e0b666e227d708aafface3a
Caue Assmann <caue@madesa.com>
2025-04-02 13:17:26 -0300

    feat: import masks

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { vMaska } from 'maska/vue'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.mount('#app')
+app.directive('maska', vMaska);
+app.mount('#app');
